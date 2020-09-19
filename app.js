47c1ee0ba1ecce048831fbbf23b0c95918d5c6eb
Promise Fru <promisefru1@gmail.com>
2020-09-19 20:01:52 +0100

    file upload working with express-filesupload

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var upload = require('express-fileupload');
 var app = express();
 const port = 3000;
 
+app.use(upload());
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
+})
+
+app.post('/api/fileanalyse', (req, res) => {
+    if(!req.files) {
+        res.json({
+            error: "No file uploaded"
+        });
+    }else {
+        console.log(req.files.upfile);
+    }
 })
 
 app.listen(port, console.log(`listening on port ${port}`));
